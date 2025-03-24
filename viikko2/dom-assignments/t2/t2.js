7db38f9e484fd1f5682047815c8b5f2a01f283cc
leevi-rinnetmaki <leevi.rinnetmaki@hotmail.com>
2025-03-24 08:05:07 +0200

    Assignment 2 done

--- viikko2/dom-assignments/t2/t2.js
+++ viikko2/dom-assignments/t2/t2.js
@@ -28,3 +28,24 @@ const todoList = [
 ];
 
 // add your code here
+const list = document.querySelector("ul");
+
+todoList.forEach(i => {
+  let listItem = document.createElement("li");
+  let check = document.createElement("input");
+  let task = document.createElement("label");
+
+  let checkNode = document.createTextNode(i.task);
+  check.appendChild(checkNode);
+  check.setAttribute("type", "checkbox");
+  check.setAttribute("id", i.id);
+  if (i.completed) check.checked=true;
+  
+  let taskNode = document.createTextNode(i.task);
+  task.appendChild(taskNode);
+  task.setAttribute("for", i.id);
+
+  listItem.appendChild(check);
+  listItem.appendChild(task);
+  list.appendChild(listItem);
+})
