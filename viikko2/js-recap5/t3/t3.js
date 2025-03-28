5058706bb90b53f052d4593aba5c8964f7d0e08f
leevi-rinnetmaki <leevi.rinnetmaki@hotmail.com>
2025-03-28 16:06:48 +0200

    week2 recap5 t3 finished

--- viikko2/js-recap5/t3/t3.js
+++ viikko2/js-recap5/t3/t3.js
@@ -1,17 +1,25 @@
 async function errorFunction(){
     try{
-        const thisWorks = await fetch("https://reqres.in/api/users/1", {method: "GET"});
-        console.log(thisWorks);
-
         const getError = await fetch("https://reqres.in/api/unknown/23");
         const postError = await fetch("https://reqres.in/api/unknown/23", {method: "POST"});
         const putError = await fetch("https://reqres.in/api/unknown/23", {method: "PUT"});
         const deleteError = await fetch("https://reqres.in/api/unknown/23", {method: "DELETE"});
 
-
+        if(!getError.ok){
+            throw new Error(`Error ${getError.status}: ${getError.statusText}`);
+        }
+        if(!postError.ok){
+            throw new Error(`Error ${postError.status}: ${postError.statusText}`);
+        }
+        if(!putError.ok){
+            throw new Error(`Error ${putError.status}: ${putError.statusText}`);
+        }
+        if(!deleteError.ok){
+            throw new Error(`Error ${deleteError.status}: ${deleteError.statusText}`);
+        }
+        
     }catch(error){
         console.log(error);
     }
 }
-
 errorFunction();
