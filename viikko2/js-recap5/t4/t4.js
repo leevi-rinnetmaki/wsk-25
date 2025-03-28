77876a2fa396cf2b7ade6c51d9e10ba611c9d0f7
leevi-rinnetmaki <leevi.rinnetmaki@hotmail.com>
2025-03-28 17:12:31 +0200

    week2 recap5 t4 done

--- viikko2/js-recap5/t4/t4.js
+++ viikko2/js-recap5/t4/t4.js
@@ -1,3 +1,21 @@
-async function fetchData(){
+async function fetchData(url, options) {
+    try {
+        const response = await fetch(url, options);
 
+        if (!response.ok) {
+            throw new Error(`Error ${response.status}: ${response.statusText}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+    }
 }
+
+const data = fetchData("https://reqres.in/api/users", {method: "POST"})
+console.log(data);
+
+async function pleaseJustPrintIt(print){
+    console.log(await print);
+}
+pleaseJustPrintIt(data);
